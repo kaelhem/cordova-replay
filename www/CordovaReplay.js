e8ab1fad0d6ed46609695952dfaafdd2f6526eb5
Hernan Zhou <hernanzh@gmail.com>
2017-10-26 16:52:58 +0200

    add live broadcasting options

--- www/CordovaReplay.js
+++ www/CordovaReplay.js
@@ -1,13 +1,30 @@
 var exec = require('cordova/exec');
 
-var Replay = function() {};
-
-Replay.startRecording = function(isMicrophoneEnabled, success, error) {
-    exec(success, error, "CordovaReplay", "startRecording", [isMicrophoneEnabled]);
-};
-
-Replay.stopRecording = function(success, error) {
-    exec(success, error, "CordovaReplay", "stopRecording");
+var Replay = {
+    isAvailable: function (success, error) {
+        exec(success, error, 'CordovaReplay', 'isAvailable');
+    },
+    startRecording: function (isMicrophoneEnabled, success, error) {
+        exec(success, error, "CordovaReplay", "startRecording", [isMicrophoneEnabled]);
+    },
+    stopRecording: function (success, error) {
+        exec(success, error, "CordovaReplay", "stopRecording");
+    },
+    isRecording: function (success, error) {
+        exec(success, error, "CordovaReplay", "isRecording");
+    },
+    startBroadcast: function (success, error) {
+        exec(success, error, "CordovaReplay", "startBroadcast");
+    },
+    stopBroadcast: function (success, error) {
+        exec(success, error, "CordovaReplay", "stopBroadcast");
+    },
+    isBroadcasting: function (success, error) {
+        exec(success, error, "CordovaReplay", "isBroadcasting");
+    },
+    isBroadcastAvailable: function (success, error) {
+        exec(success, error, "CordovaReplay", "isBroadcastAvailable");
+    }
 };
 
 module.exports = Replay;
